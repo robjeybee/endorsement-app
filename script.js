e1042359faeae6eee081fab563f83ed735e1ac46
Rob B <rob@rjrobbie.dev>
2024-02-26 13:55:56 +0000

    setup boilerplate HTML, CSS and JS files and styled app

---
+++ script.js
@@ -0,0 +1,7 @@
+const inputEl = document.getElementById("input-el");
+const btnEl = document.getElementById("btn-el");
+const listEl = document.getElementById("list-el");
+
+btnEl.addEventListener("click", function () {
+  console.log("Button clicked");
+});
